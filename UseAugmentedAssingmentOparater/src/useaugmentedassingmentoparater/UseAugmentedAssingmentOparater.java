1c74a9a35e18e8d9890a3bffdbbf82579dd4127d
mdalimortuzamasud-lab <md.alimortuzamasud@gmail.com>
2026-01-26 18:59:39 +0600

    add

---
+++ UseAugmentedAssingmentOparater/src/useaugmentedassingmentoparater/UseAugmentedAssingmentOparater.java
@@ -0,0 +1,46 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package useaugmentedassingmentoparater;
+
+/**
+ *
+ * @author Admin
+ */
+public class UseAugmentedAssingmentOparater {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int i = 90;
+        
+        System.out.println( "I++ : " + i++);
+        System.out.println( "I++ : " + ++i);
+        System.out.println( "I++ : " + --i);
+        System.out.println( "I++ : " + i++);
+        System.out.println( "I++ : " + --i);
+        System.out.println( "I++ : " + --i);
+        System.out.println( "I++ : " + i++);
+        System.out.println( "I++ : " + --i);
+        System.out.println( "I++ : " + i++);
+        
+//        int assingt = 10;
+//        System.out.println(assingt+=8);
+//        System.out.println(assingt-=8);
+//        System.out.println(assingt*=8);
+//        System.out.println(assingt/=8);
+//        System.out.println(assingt%=8);
+        
+        
+        
+                
+        
+        
+        
+        
+    }
+    
+}
