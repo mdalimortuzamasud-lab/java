f42512cd110bcfd885ab32bcd16cc94a23768c1f
mdalimortuzamasud-lab <md.alimortuzamasud@gmail.com>
2026-02-02 19:08:08 +0600

    fdgdfgf

---
+++ JavaApplication14/src/javaapplication14/JavaApplication14.java
@@ -0,0 +1,32 @@
+
+package javaapplication14;
+
+
+public class JavaApplication14 {
+
+    public static void main(String[] args) {
+        String studentsName [] = new String[14];
+        int studentsAge[]= new int[14];
+        
+        
+        studentsName[0]="badrul";
+        studentsName[1]="shimul";
+        studentsName[4]="hasan";
+                
+         studentsAge[0]=25;
+         studentsAge[1]=20;
+         studentsAge[3]=23;
+       
+         System.out.println("studentsName");
+         System.out.println("studentsAge");
+         
+         
+         System.out.println(Arrays);
+         
+         
+         
+                           
+        
+    }
+    
+}
