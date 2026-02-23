bd97a5ffe42cbfeb8b29fdf6184c391d41c8496f
mdalimortuzamasud-lab <md.alimortuzamasud@gmail.com>
2026-02-23 16:31:55 +0600

    ggrg

---
+++ StringNumber/src/stringnumber/StringNumber.java
@@ -0,0 +1,16 @@
+package stringnumber;
+
+import java.util.ArrayList;
+import newpackage.in.Student;
+
+public class StringNumber {
+
+    public static void main(String[] args) {
+        ArrayList<Student> slist = new ArrayList<>();
+    
+               
+        slist.add(new Student(1, "abuyl", "abul@gmail.com"));
+        System.out.println(slist);
+    }
+
+}
